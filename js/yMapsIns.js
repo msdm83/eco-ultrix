567a17edf21b615a78d4c30f8b1887ec906d3ad8
msdm83 <56660235+msdm83@users.noreply.github.com>
2019-11-25 13:15:45 +0300

    Add points page
    add points page with yandex maps. add points for Vsevolzsk-part1. Use yandex instead of google in whole site

---
+++ js/yMapsIns.js
@@ -0,0 +1,105 @@
+//points page map initialization
+function init2() {
+    var myMap = new ymaps.Map('map2', {
+            center: [60.01,30.67],
+            zoom: 11,
+            controls: ['zoomControl']
+        });
+
+      //ymaps.geocode('Всеволжск ул  Горсткина д. 14').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+
+  ymaps.geocode('всеволжск	Добровольского 16/15	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Невская 11	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Московская 26/28	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Московская 19/5	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Невская 1/2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Малиновского 6	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Знаменская 1/8	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Крымская 4	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Московская 27/5	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Центральная 10	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Центральная 10/1	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Московская 30	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Добровольского 20/1	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Доктора Сотникова 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Солнечная 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Доктора Сотникова 13	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Доктора Сотникова 29	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Южная Поляна	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Южная Поляна	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	ЖК Династия	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Центральная 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 12	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Взлетная 12	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Колтушское 19	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Токкари Лэнд	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Токкари Лэнд	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Доктора Сотникова 25/27	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Комсомола 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Комсомола 7	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Комсомола 9	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Лубянская 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Лубянская 4	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	КП Кенша	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	ДНП Дружное	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	СНТ Родничковое	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Снт Придорожное 1	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Снт Придорожное 2	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 1	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 4	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 5	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 8	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Аэропортовская 12	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Центральная 11	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Московская 12	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Невская 6	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Народная 8	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+  ymaps.geocode('всеволжск	Невская 12	').then(function (res) { myMap.geoObjects.add(res.geoObjects);});
+
+
+
+}
+ymaps.ready(init2);
+
+
+
+//contact page map initialization
+function init1() {
+    var myMap = new ymaps.Map('map1', {
+            center: [59.94079856, 30.390491],
+            zoom: 11,
+            controls: ['zoomControl']
+        });
+
+        var myPlacemark = new ymaps.Placemark([59.94079856, 30.390491], {
+            balloonContentHeader: 'Новгородская ул., 23',
+            balloonContentBody: 'Новгородская ул., 23, Санкт-Петербург, 191124',
+            hintContent: 'Новгородская ул., 23',
+            balloonContent:'Новгородская ул., 23',
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+        myPlacemark.balloon.open();
+}
+ymaps.ready(init1);
+/*// Создаем коллекцию.
+  //  myCollection = new ymaps.GeoObjectCollection(),
+// Создаем массив с данными.
+    myPoints = [
+        { coords: [60.01, 30.675], text: 'Трактир' },
+        { coords: [60.01, 30.678], text: 'Кафе' },
+        { coords: [60.02, 30.54], text: 'Ресторан' },
+        { coords: [60.01, 30.676], text: 'Музей' },
+        { coords: [60.03, 30.671], text: 'Библиотека' },
+        { coords: [60.04, 30.672], text: 'Школа' },
+    ];
+    // Заполняем коллекцию данными.
+    for (var i = 0, l = myPoints.length; i < l; i++) {
+        var point = myPoints[i];
+        myCollection.add(new ymaps.Placemark(
+            point.coords, {
+                balloonContentBody: point.text
+            }
+        ));
+    }
+    //myMap.geoObjects.add(myCollection);*/
